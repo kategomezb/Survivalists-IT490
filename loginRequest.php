535d5641512ea8bb81a251020d032e40cba3db85
edgarsteven <eso@njit.edu>
2026-03-11 10:58:09 -0400

    fixed href and other links to link correctly, renamed css to match html

--- loginRequest.php
+++ loginRequest.php
@@ -1,7 +1,7 @@
 <?php
-require_once('/var/www/includes/path.inc');
-require_once('/var/www/includes/get_host_info.inc');
-require_once('/var/www/includes/rabbitMQLib.inc');
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
 
 $client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
 
--- reviewRequest.php
+++ reviewRequest.php
@@ -3,9 +3,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
 
-require_once('/var/www/includes/path.inc');
-require_once('/var/www/includes/get_host_info.inc');
-require_once('/var/www/includes/rabbitMQLib.inc');
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
 
 $client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
 
