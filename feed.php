2da35916629e8f684170d9ac16007d46808c5e98
Kim Corpuz <kc648@njit.edu>
2026-03-14 08:42:57 +0000

    applied the same fix to the post object array retrieval bug like on user-profile.php and added the nested foreach loop logic for retrieving followed users' posts on feed

--- feed.php
+++ feed.php
@@ -1,19 +1,18 @@
 <?php
-    require 'vendor/autoload.php'; 
+require 'vendor/autoload.php';
 
-	if (!isset($_COOKIE['SessionKey'])) { // WEB REFERENCE USED: https://www.geeksforgeeks.org/php/php-cookies/
-		header('Location: login.html');
-		exit();
-	} else {
-        $uri = "mongodb://100.105.160.23:27017/";
-    
-        $client = new MongoDB\Client($uri);
-        $database = $client->survivalists_db;
-        $userCollection = $database->reg_users;
-        
-        $user = $userCollection->findOne(['keySession' => $_COOKIE['SessionKey']]);
+if (!isset($_COOKIE['SessionKey'])) { // WEB REFERENCE USED: https://www.geeksforgeeks.org/php/php-cookies/
+    header('Location: login.html');
+    exit();
+} else {
+    $uri = "mongodb://100.105.160.23:27017/";
 
-    }
+    $client = new MongoDB\Client($uri);
+    $database = $client->survivalists_db;
+    $userCollection = $database->reg_users;
+
+    $user = $userCollection->findOne(['keySession' => $_COOKIE['SessionKey']]);
+}
 ?>
 
 <!-- video ref used for basic page template: https://www.youtube.com/watch?v=NljIHlZRTTE (PT 1) -->
@@ -37,7 +36,7 @@
         <div class="nav-left">
             <img src="../images/sample.png" alt="logo" class="logo">
 
-            <!-- will eventually adjust nav items accordingly for future deliverables AKA these are just placeholders for now -->
+            <!-- will eventually documentadjust nav items accordingly for future deliverables AKA these are just placeholders for now -->
             <!-- will use fontawesome icons for navbars -->
 
             <ul>
@@ -59,71 +58,81 @@
     </nav>
 
     <!-- + button that will redirect user to search page -->
-     <a href="/searchBar.php" class="href">Make a new post</a>
+    <a href="/searchBar.php" class="href">Make a new post</a>
 
 
     <!-- profile page -->
     <div class="profile-container">
-<?php
-                    $userPosts = $user['posts'];
-                    foreach($userPosts as $document) {
-                    echo "<div class='post-container'>";
-                        echo "<div class='post-row'>";
-                            echo "<div class='user-profile'>";
-                                echo "<i class='fa-solid fa-circle-user'>";
-                                echo "</i>";
-                                // <!-- will modify to user icons instead of images -->
-                                echo "<div>";
-                                    // <!-- <p>RETRIEVE USERNAME FROM POST DATABASE</p> -->
-                                    
-                                    echo "<p>";
-                                    
-                                    // RETRIEVE USERNAME BY LOOKING UP STORED SESSION KEY
-                                    $username = $user['username'];
-                                    $media = json_encode($userPosts['media']);
-                                    $content = json_encode($userPosts['content']);                             
-                                    $postedAt = json_encode($userPosts['postedAt']);
+        <?php
+        // need to access each of the users in the logged in user's following array
+        $followingList = $user['following'];
 
-                                    echo $username;
-                                    
-                                    echo "</p>";    
-                                    
-                                    echo "<span>";
+        // for each user in the following array
+        foreach ($followingList as $userFollowed) {
 
-                                    echo $postedAt;
+            // get the current user's posts array that the pointer is pointing at during iterating round
+            // following feed in this case will not include the logged in user's personal posts
+            $userPosts = $userFollowed['posts'];
 
-                                    echo "</span>";
-                                                
-                                    // RETRIEVE DATE OBJECT FROM LOGGED IN USER'S POSTS ARRAY BY ITERATING W/ FOREACH LOOP
-                                    
+            foreach ($userPosts as $postsMadeByUserFollowed) {
+                echo "<div class='post-container'>";
+                echo "<div class='post-row'>";
+                echo "<div class='user-profile'>";
+                echo "<i class='fa-solid fa-circle-user'>";
+                echo "</i>";
+                // <!-- will modify to user icons instead of images -->
+                echo "<div>";
+                // <!-- <p>RETRIEVE USERNAME FROM POST DATABASE</p> -->
 
-                                    echo "<p class='post-text'>";
-                                    echo $media;
-                                    echo $content;
-                                    echo "</p>";
+                echo "<p>";
 
-                                echo "</div>";
-                            echo "</div>";
-                            echo "<a href='#'>";
-                            echo "<i class='fas fa-ellipsis-v'></i></a>";
-                        echo "</div>";
+                // RETRIEVE USERNAME BY LOOKING UP STORED SESSION KEY
+                $posterUsername = $postsMadeByUserFollowed->username;
+                $media = $postsMadeByUserFollowed->media;
+                $content = $postsMadeByUserFollowed->content;
+                $postedAt = $postsMadeByUserFollowed->postedAt;
 
-                        // <!-- <p class="post-text">RETRIEVE USERNAME FROM POST DATABASE</p> -->
-                        
-                        // <div class="post-row">
-                        //     <div class="activity-icons">
-                        //         <div><img src="../images/like.png" alt="like"> RETRIEVE COUNTER OBJECT FROM POST DATABASE
-                        //         </div>
-                        //         <div><img src="../images/comments.png" alt="comments"> RETRIEVE COUNTER OBJECT FROM POST
-                        //             DATABASE</div>
-                        //         <div><img src="../images/share.png" alt="shares"> RETRIEVE COUNTER OBJECT FROM POST DATABASE
-                        //         </div>
+                echo $posterUsername;
 
-                        //     </div>
-                        // </div>
-                        echo "</div>";
-                    };
-                ?>
+                echo "</p>";
+
+                echo "<span>";
+
+                echo $postedAt;
+
+                echo "</span>";
+
+                // RETRIEVE DATE OBJECT FROM LOGGED IN USER'S POSTS ARRAY BY ITERATING W/ FOREACH LOOP
+
+
+                echo "<p class='post-text'>";
+                echo $media;
+                echo $content;
+                echo "</p>";
+
+                echo "</div>";
+                echo "</div>";
+                echo "<a href='#'>";
+                echo "<i class='fas fa-ellipsis-v'></i></a>";
+                echo "</div>";
+
+                // <!-- <p class="post-text">RETRIEVE USERNAME FROM POST DATABASE</p> -->
+
+                // <div class="post-row">
+                //     <div class="activity-icons">
+                //         <div><img src="../images/like.png" alt="like"> RETRIEVE COUNTER OBJECT FROM POST DATABASE
+                //         </div>
+                //         <div><img src="../images/comments.png" alt="comments"> RETRIEVE COUNTER OBJECT FROM POST
+                //             DATABASE</div>
+                //         <div><img src="../images/share.png" alt="shares"> RETRIEVE COUNTER OBJECT FROM POST DATABASE
+                //         </div>
+
+                //     </div>
+                // </div>
+                echo "</div>";
+            };
+        };
+        ?>
     </div>
 
     <div class="footer">
